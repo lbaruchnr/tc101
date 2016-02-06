003e5f70ff90ea48d343524c3fa59a87c058a523
lbaruchnr <lbaruchnr@gmail.com>
2016-02-06 14:04:07 -0600

    Create prog2.cpp

---
+++ prog2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int a, b, r;
+
+cout<<"dame el primer numero"<<endl;
+cin>>a;
+cout<<"dame el segundo numero"<<endl;
+cin>>b;
+r=a*b;
+cout<<"product "<<r<<endl;
+r=a/b;
+cout<<"division "<<r<<endl;
+r=a%b;
+cout<<"remainder "<<r<<endl;
+
+return 0;
+}
