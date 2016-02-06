cfecae5a8b0fcda495c25925161395cfe8616161
lbaruchnr <lbaruchnr@gmail.com>
2016-02-06 14:04:36 -0600

    Create prog3.cpp

---
+++ prog3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+float a, b, r;
+
+cout<<"dame el primer numero"<<endl;
+cin>>a;
+cout<<"dame el segundo numero"<<endl;
+cin>>b;
+r=a+b;
+cout<<"addition "<<r<<endl;
+r=a-b;
+cout<<"subtraction "<<r<<endl;
+r=a*b;
+cout<<"product "<<r<<endl;
+r=a/b;
+cout<<"division "<<r<<endl;
+
+return 0;
+}
