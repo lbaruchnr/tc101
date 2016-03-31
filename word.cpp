3f6cc85715e726a53f175a63e63e60c5a730e3cd
lbaruchnr <lbaruchnr@gmail.com>
2016-03-31 11:28:57 -0600

    Create word.cpp

---
+++ word.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int find_word(string pal, string tipo){
+  std::string line;
+  int cont=0;
+  ifstream myfile(tipo);
+  //string myfile(tipo);
+  if (myfile.is_open())
+  {
+    while(getline(myfile,line)){
+      if(line.compare(pal)==0){
+        cont++;
+      }
+    }
+    myfile.close();
+  }
+    else {
+  cout<<"no se encontro o no se pudo habrir el archivo"<<endl;
+  }
+  cout<<"la palabra esta: "<<cont<<endl;
+  return cont;
+}
+
+int main(){
+  string pal, tipo, line;
+  int count;
+  cout<<"la palabra que busca"<<endl;
+  cin>>pal;
+  cout<<"ingresa el nombre y extecion o tipo de archivo que buscas"<<endl;
+  cin>>tipo;
+  find_word(pal, tipo);
+return 0;
+}
